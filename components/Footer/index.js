39f4ba6464a2768a5e1f3e6ee914bb4ce03abc0f
Fer Alva <feralvaa@hotmail.com>
2020-06-27 10:00:18 -0400

    first

---
+++ components/Footer/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./style.css";
+
+
+function Navbar() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <h1> Employee Directory</h1>
+
+
+    </nav>
+  );
+}
+
+export default Navbar;
---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import Navbar from "./components/Navbar";
+import './App.css';
+
+function App() {
+  return (
+    
+    <div className="App">
+      <Navbar />
+      <header className="App-header">
+      <p> Table </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
